64a8c86430b2d62e7e9b4c823e139a8d2a07ef31
gustavo-barrios2006 <gabarrios2006@gmail.com>
2024-06-16 00:14:44 -0300

    adicionando dica se o chute dado foi menor ou maior que o número secreto quando não acerte.

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,6 +3,9 @@ numero_secreto=42
 chute=int(input("Digite seu chute"))
 if(chute==numero_secreto):
     print("Você acertou!")
+elif chute<numero_secreto:
+    print("Você errou! O seu chute foi menor que o número secreto.")
 else:
-    print("Você errou!")
+    print("Você errou! O seu chute foi maior que o número secreto.")
+
 print("Fim do jogo")
