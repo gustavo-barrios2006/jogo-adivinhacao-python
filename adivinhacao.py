caaf7e5bb5c2dfba902a95826375fbc40c1bb07c
gustavo-barrios2006 <gabarrios2006@gmail.com>
2024-06-16 00:29:27 -0300

    Adicionando possibilidade de até três tentativas.

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,11 +1,16 @@
 print("* Bem vindo ao jogo de adivinhação *")
 numero_secreto=42
-chute=int(input("Digite seu chute"))
-if(chute==numero_secreto):
-    print("Você acertou!")
-elif chute<numero_secreto:
-    print("Você errou! O seu chute foi menor que o número secreto.")
-else:
-    print("Você errou! O seu chute foi maior que o número secreto.")
-
+total_de_tentativas=3
+rodada=1
+while(rodada<=total_de_tentativas):
+    print("Rodada", rodada, "de", total_de_tentativas)
+    chute=int(input("Digite seu chute"))
+    if(chute==numero_secreto):
+        print("Você acertou!")
+        break
+    elif chute<numero_secreto:
+        print("Você errou! O seu chute foi menor que o número secreto.")
+    else:
+        print("Você errou! O seu chute foi maior que o número secreto.")
+    rodada+=1
 print("Fim do jogo")
