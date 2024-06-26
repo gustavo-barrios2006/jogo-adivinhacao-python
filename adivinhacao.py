66d774569d7c97c5382d41b4105f4c497d93038a
gustavo-barrios2006 <gabarrios2006@gmail.com>
2024-06-26 14:27:19 -0300

    Definindo toda execução do código na função jogar e fazendo ele executar a função caso o arquivo .py seja executado separadamente para permitir a posterior reutilização desse arquivo em outro repositório que conterá um programa que pessa para o usuário escolher entre o jogo de adivinhação e o de forca para jogar seguindo o curso da Alura citado no README desse repositório.

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,30 +1,33 @@
 import random
-print("* Bem vindo ao jogo de adivinhação *")
-numero_secreto=random.randrange(1, 101)
-print("Qual o nível de dificuldade?")
-print("(1) Fácil (2) Médio (3) Difícil")
-nivel=int(input("Defina o nível: "))
-while(nivel<=0 or nivel>3):
-    nivel=int(input("Nível inválido, digite um número de 1 a 3: "))
-if(nivel==1):
-    total_de_tentativas=20
-elif(nivel==2):
-    total_de_tentativas=10
-else:
-    total_de_tentativas=5
-pontos=1000
-for rodada in range(1, total_de_tentativas+1):
-    print("Rodada {} de {}".format(rodada, total_de_tentativas))
-    chute=int(input("Digite seu chute"))
-    if(chute<1 or chute>100):
-        print("Você deve digitar um número entre 1 e 100")
-        continue
-    if(chute==numero_secreto):
-        print("Você acertou e fez {} pontos!".format(pontos))
-        break
-    elif chute<numero_secreto:
-        print("Você errou! O seu chute foi menor que o número secreto.")
+def jogar():
+    print("* Bem vindo ao jogo de adivinhação *")
+    numero_secreto=random.randrange(1, 101)
+    print("Qual o nível de dificuldade?")
+    print("(1) Fácil (2) Médio (3) Difícil")
+    nivel=int(input("Defina o nível: "))
+    while(nivel<=0 or nivel>3):
+        nivel=int(input("Nível inválido, digite um número de 1 a 3: "))
+    if(nivel==1):
+        total_de_tentativas=20
+    elif(nivel==2):
+        total_de_tentativas=10
     else:
-        print("Você errou! O seu chute foi maior que o número secreto.")
-    pontos-=abs(chute-numero_secreto)
-print("Fim do jogo")
+        total_de_tentativas=5
+    pontos=1000
+    for rodada in range(1, total_de_tentativas+1):
+        print("Rodada {} de {}".format(rodada, total_de_tentativas))
+        chute=int(input("Digite seu chute"))
+        if(chute<1 or chute>100):
+            print("Você deve digitar um número entre 1 e 100")
+            continue
+        if(chute==numero_secreto):
+            print("Você acertou e fez {} pontos!".format(pontos))
+            break
+        elif chute<numero_secreto:
+            print("Você errou! O seu chute foi menor que o número secreto.")
+        else:
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        pontos-=abs(chute-numero_secreto)
+    print("Fim do jogo")
+if(__name__=="__main__"):
+    jogar()
